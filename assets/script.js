f43eb301fe4d16de4a8b5429794a231900f8ff5d
Joshua Yuan <joshua43210@gmail.com>
2019-01-02 13:52:14 -0800

    highlight sidebar link

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
-  var $grid = $('.grid').imagesLoaded().progress(function() {
-    $grid.isotope({
-      itemSelector: '.grid-item',
+  // Media isotope grid
+  var $media_grid = $('#media .grid').imagesLoaded().progress(function() {
+    $media_grid.isotope({
+      itemSelector: '#media .grid-item',
       masonry: {
         columnWidth: 106.66666,
         gutter: 5
       }
     });
   });
+
+  // Highlighting the sidebar text
+  var pathname = window.location.pathname;
+  var ind = pathname.indexOf("/", 1);
+  var filename = pathname.substring(ind + 2, pathname.length-5);
+  if (filename.length <= 1) {
+    filename = "news";
+  }
+  $("#" + filename + "-bar a").addClass("active");
 });
