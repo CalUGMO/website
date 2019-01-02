773efd4ce39fafda90ae98d279fa662bd1ae80ae
Joshua Yuan <joshua43210@gmail.com>
2019-01-02 13:10:36 -0800

    media page

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-  $('.grid').isotope({
-    itemSelector: '.grid-item',
-    layoutMode: 'fitRows'
+  var $grid = $('.grid').imagesLoaded().progress(function() {
+    $grid.isotope({
+      itemSelector: '.grid-item',
+      masonry: {
+        columnWidth: 106.66666,
+        gutter: 5
+      }
+    });
   });
 });
